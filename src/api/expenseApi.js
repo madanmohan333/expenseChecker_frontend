617bb474693dc2d030ebc02e30880fa18124cf76
madanmohan333 <shivaclasses406@gmail.com>
2026-02-19 00:27:36 +0530

    backendURI updated

--- src/api/expenseApi.js
+++ src/api/expenseApi.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    "https://fenmobackend.onrender.com"
+    "https://expensechecker-backend.onrender.com"
 });
 
 export const createExpense = (data) => API.post("/expenses", data);
