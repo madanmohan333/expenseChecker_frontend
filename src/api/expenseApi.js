d1ac298e2ad6a7dc31e6477aa237103858628860
madanmohan333 <shivaclasses406@gmail.com>
2026-02-19 00:11:22 +0530

    expense API created

---
+++ src/api/expenseApi.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://fenmobackend.onrender.com"
+});
+
+export const createExpense = (data) => API.post("/expenses", data);
+export const getExpenses = (params) => API.get("/expenses", { params });
+export const deleteExpense = (id) => API.delete(`/expenses/${id}`);
